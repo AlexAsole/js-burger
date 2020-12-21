dba1e41f558dfb717f22313e1f9e67ab243484ab
Alex Giorgio Asole <alexgiorgio.asole@gmail.com>
2020-12-21 15:54:17 +0100

    Fase 3: calcolo prezzo base, senza sconto

--- js/script.js
+++ js/script.js
@@ -1,23 +1,33 @@
-// fase 1: se non scrivo un nome per il panino non vado avanti
 var discount = ['Mickey', 'Donald', 'Goofy'];
 var nomeBurger = document.getElementsByClassName('name-burger')[0];
 var checkIngredients = document.getElementsByClassName('container')[0].getElementsByTagName('input');
-var counterIngredients = 0;
+var inputDiscount = document.getElementsByClassName('discount')[0];
 var btn = document.getElementsByTagName('button')[0];
+var finalPrice = document.getElementsByClassName('price')[0];
+var basePrice = 50;
+var counterIngredients = 0;
+var checkedIngredients = [];
 
-btn.addEventListener('click', function(){
+
+finalPrice.innerText = basePrice;
+
+btn.addEventListener('click', function(){ // fase 1: se non scrivo un nome per il panino non vado avanti
   if (nomeBurger.value === ''){
     alert('Inserisci il nome, per favore!')
   } else { //fase 2: se non seleziono almeno 2 checkbox si blocca
     for (var i = 0; i < checkIngredients.length; i++) {
       if (checkIngredients[i].checked) {
+        checkedIngredients.push(checkIngredients[i].value);
         counterIngredients +=1;
       }
     }
     if (counterIngredients < 2) {
       alert('Seleziona almeno 2 ingredienti!')
-    } else {
-      alert('Vai avanti!')
+    } else { //fase 3: calcolo il prezzo base
+      for (x = 0; x < checkedIngredients.length; x++){
+        basePrice += parseInt(checkedIngredients[x])
+      }
+      console.log(basePrice)
     }
   }
 
