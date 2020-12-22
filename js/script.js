d5c2f808b74ee9e78e4a20b75ab24b604818b61a
Alex Giorgio Asole <alexgiorgio.asole@gmail.com>
2020-12-22 12:06:48 +0100

    js reworkato

--- js/script.js
+++ js/script.js
@@ -1,48 +1,38 @@
 var discount = ['Mickey', 'Donald', 'Goofy'];
+var basePrice = 50;
+var minIng = 2;
+var disValue = 0.2;
 var nomeBurger = document.getElementsByClassName('name-burger')[0];
 var checkIngredients = document.getElementsByClassName('container')[0].getElementsByTagName('input');
 var inputDiscount = document.getElementsByClassName('discount')[0];
 var btn = document.getElementsByTagName('button')[0];
 var finalPrice = document.getElementsByClassName('price')[0];
-var basePrice = 50;
 var counterIngredients = 0;
-var checkedIngredients = [];
-var flag
+var addPrice = basePrice;
 
-finalPrice.innerText = '$ ' + basePrice
+finalPrice.innerText = '$ ' + addPrice;
 
 btn.addEventListener('click', function(){ // fase 1: se non scrivo un nome per il panino non vado avanti
   if (nomeBurger.value.length === 0){
-    finalPrice.innerText = '$ ' + basePrice;
     alert('Inserisci il nome, per favore!')
-  } else { //fase 2: se non seleziono almeno 2 checkbox si blocca
+    finalPrice.innerText = '$ ' + addPrice;
+  } else { //fase 2: se non seleziono almeno 2 checkbox si blocca*
     for (var i = 0; i < checkIngredients.length; i++) {
       if (checkIngredients[i].checked) {
-        checkedIngredients.push(checkIngredients[i].value);
-        counterIngredients +=1;
+        addPrice += parseInt(checkIngredients[i].value); //fase 3: calcolo prezzo base
+        counterIngredients++;
       }
     }
-    if (counterIngredients < 2) {
+    if (counterIngredients < minIng) { //*se non hai selezionato 2 check, si blocca
       alert('Seleziona almeno 2 ingredienti!')
-      finalPrice.innerText = '$ ' + basePrice;
-    } else { //fase 3: calcolo il prezzo base
-      for (x = 0; x < checkedIngredients.length; x++){
-        basePrice += parseInt(checkedIngredients[x])
+      finalPrice.innerText = '$ ' + addPrice;
+    } else {
+      if(discount.indexOf(inputDiscount.value) !== -1){
+        addPrice -= addPrice*disValue;
       }
-      for (var y = 0; y < discount.length; y++) { //fase 4: Controlliamo lo sconto
-        if(inputDiscount.value === discount[y]){
-          flag = true;
-        }
-      }
-      if(flag) {
-        basePrice = basePrice - (basePrice * 0.2);
-      }
-      finalPrice.innerText = '$ ' + basePrice;
     }
-  }
-  //fase 5: resetto le var alla base
-  basePrice = 50;
-  counterIngredients = 0;
-  checkedIngredients = [];
-  flag = false;
+    finalPrice.innerText = '$ ' + addPrice;
+}
+counterIngredients = 0;
+addPrice = basePrice;
 })
